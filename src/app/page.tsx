b1c9acd27b3b979880c19971261a7cc1348951b2
Mahbubur Rahma <inbx.mahbub@gmail.com>
2024-09-17 18:52:29 +0600

    created development branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function page() {
   return (
     <div className='text-center mt-20 text-5xl'>
-      ❤️ Welcome to <span className='text-blue-400 font-bold capitalize'>way go</span> ..❤️
+      ❤️ Welcome to <span className='text-blue-400 font-bold capitalize'>way go ...</span> ..❤️
     </div>
   )
 }
