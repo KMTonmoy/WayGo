08aaa0c54b21323113b5fd0ab695297f9461bbbe
Mahbubur Rahma <inbx.mahbub@gmail.com>
2024-09-17 18:43:48 +0600

    Ready to expand the WayGo

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='text-center mt-20 text-5xl'>
+      ❤️ Welcome to <span className='text-blue-400 font-bold capitalize'>way go</span> ..❤️
+    </div>
+  )
+}
+
+export default page
