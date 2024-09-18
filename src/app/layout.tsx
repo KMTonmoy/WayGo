2a5ebd9dc772cbf9a78d8ca63be340e3cb0116ab
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-09-19 01:40:56 +0600

    File Path Changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "@/Components/Navbar/Navbar";
-import Footer from "@/Components/Footer";
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer";
+ 
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
