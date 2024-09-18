b56e3ed05445ecf5fb78cf04a2191a0276ec8480
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-09-18 22:56:39 +0600

    Banner Updated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -16,7 +16,7 @@ const LoginPage: React.FC = () => {
                             className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-[#f0652b]"
                             placeholder="Your Email"
                         />
-                    </div>
+                    </div> 
 
                     <div className="mb-4">
                         <label className="block text-gray-700 mb-2" htmlFor="password">Password</label>
