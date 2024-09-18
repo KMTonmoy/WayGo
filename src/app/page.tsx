ba5a494534c0a1c15e87d6eebbb7dd86d6b079ce
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-09-18 23:38:33 +0600

    Banner Bug Fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,13 @@ const page = () => {
   return (
     <>
       <header>
-
-
+  
         <Banner />
 
         {/* banner section */}
         <div className='bg-blue-100 '>
           <div className='py-10 md:py-12 lg:py-16 max-w-[1440px] mx-auto px-4 md:px-8 lg:px-10 2xl:px-14 border-2 border-blue-500'>
-            <h1>Heading 1</h1>
+            <h1>Heading 110</h1>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea nostrum recusandae magnam quisquam nulla tenetur distinctio quidem architecto id eum enim fugiat dolorem harum eaque tempora veritatis et, beatae fugit provident asperiores error magni? Numquam exercitationem eum iure iste cupiditate quos doloribus nostrum aspernatur assumenda. Doloribus laboriosam amet vero id.
           </div>
         </div>
