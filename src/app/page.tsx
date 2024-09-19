3ee0a95942c2566414926aac0842b866f04b0e94
Mahbubur Rahma <inbx.mahbub@gmail.com>
2024-09-20 00:50:57 +0600

    dist folder added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import Banner from '../Components/Banner/Banner';
 import WhyChooseUs from '../Components/WhyChooseUs';
 import Testimonial from '../Components/Testimonial';
 import CustomSlider from '../Components/CustomSlider/CustomSlider';
 import AboutShuttle from '../Components/AboutShuttle/About';
 import OurServices from '../Components/OurServices/OurServices';
+import Banner from '../Components/Banner/banner';
 
 const Page = () => {
   return (
     <div className='flex flex-col gap-10'>
       <Banner />
-      <AboutShuttle />
-      <WhyChooseUs />
-      <OurServices />
-      <CustomSlider />
-      <Testimonial />
+      <main>
+        {/* About shuttle section */}
+        <section>
+        <AboutShuttle />
+        </section>
+        <WhyChooseUs />
+        <OurServices />
+        <CustomSlider />
+        <Testimonial />
+      </main>
     </div>
   );
 
