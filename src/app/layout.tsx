f653d8c93f07779495337a1ae84391a6cf8c87be
Mahbubur Rahma <inbx.mahbub@gmail.com>
2024-09-26 23:45:24 +0600

    error fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
 
-
         <AuthProvider>
           <div className="mb-20">
             <Navbar />
