4846d7b4b0108c951c0f712a402688673db22158
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-10-19 11:48:01 +0600

    Calculation Updated

--- src/Components/CustomSlider/CustomSlider.jsx
+++ src/Components/CustomSlider/CustomSlider.jsx
@@ -10,7 +10,7 @@ const CustomSlider = () => {
   useEffect(() => {
     const fetchDeals = async () => {
       try {
-        const res = await fetch("/hotdeal.json");
+        const res = await fetch("https://way-go-backend.vercel.app/coupons");
         const data = await res.json();
         setDeals(data);
       } catch (err) {
@@ -57,33 +57,37 @@ const CustomSlider = () => {
           {deals.map((item, idx) => (
             <motion.div
               key={idx}
-              className="flex-shrink-0 w-full md:w-1/2 lg:w-1/3 p-6 bg-gradient-to-r from-white via-gray-100 to-white shadow-lg rounded-lg relative mx-4"
+              className="flex-shrink-0 px-5 w-full md:w-1/2 lg:w-1/3 p-6 bg-gradient-to-r from-white via-gray-100 to-white shadow-lg rounded-lg relative mx-4"
               initial={{ opacity: 0, scale: 0.8 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{ duration: 0.5 }}
             >
-              <div className="flex flex-col items-center">
+              <div className="flex flex-col py-5 items-center">
                 <img
                   className="w-32 h-32 rounded-full border-4 border-[#22C55E] mb-4 shadow-lg transform transition hover:scale-105"
                   src={item.img}
                   alt={item.title}
                 />
                 <h6 className="text-2xl font-bold text-gray-800 mt-2">
-                  {item.title}
+                  Unlock an Amazing
+                  <span className="text-[#22C55E] mx-2 font-bold">
+                    {item.discountPercentage}%
+                  </span>
+                  Discount Just for You!
                 </h6>
                 <p className="text-gray-600 mt-2 text-center">
-                  {item.description}
+                  Purchase any ticket online and enjoy an exclusive flat
+                  discount of{" "}
+                  <span className="text-[#22C55E]">
+                    {item.discountPercentage}
+                  </span>
+                  %! Don’t miss out on this fantastic opportunity to save on
+                  your next adventure!
                 </p>
-                <p className="text-sm text-gray-500 mt-2">
-                  Offer valid till:{" "}
-                  <span className="font-semibold">{item.validTill}</span>
-                </p>
-                <div className="mt-4 bg-orange-200 text-[#22C55E] rounded-lg px-4 py-1 text-xs font-medium">
+
+                <div className="mt-4 bg-[#22c55e2d] text-[#22C55E] rounded-lg px-4 py-1 text-xs font-medium">
                   Promo Code: {item.promoCode}
                 </div>
-                <button className="bg-[#22C55E] mt-6 px-6 py-2 rounded-lg text-white hover:bg-[#16a34a] transition duration-300 shadow-lg transform hover:scale-105">
-                  Learn More
-                </button>
               </div>
             </motion.div>
           ))}
